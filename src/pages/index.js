8a3f5e99485cc5b22d25fdd959118c9b6c041c11
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2021-01-07 23:29:58 +0100

    añadido encabezado home

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,34 +3,44 @@ import NavBar from "../components/navBar";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
 import "../styles/home.css";
-let Home =(props)=>{
+let Home = (props) => {
+  console.log(props);
+  return (
+    <>
+      <NavBar />
+      <section>
+        <div className="containerTextHeader">
+          <div className="containerTitleText">
+            <h1>¡Somos tu frutería online!</h1>
+          </div>
+          <div className="containerTitleText">
+            <h1>¡Somos tu frutería online!</h1>
+          </div>
+          <div className="containerTitleText">
+            <h1>¡Somos tu frutería online!</h1>
+          </div>
+        </div>
 
-console.log(props)
-    return (
-      <>
-
-    <NavBar />
-    <section><Img
-        fluid={props.data.file.childImageSharp.fluid}
-        alt="frutas y verduras"
-      /></section>
-    
+        <Img
+          className="titleImage"
+          fluid={props.data.file.childImageSharp.fluid}
+          alt="frutas y verduras"
+        />
+      </section>
     </>
-    )
-  
-}
+  );
+};
 
 export const query = graphql`
   query MyQuery {
-    file(relativePath: { eq: "frutas-y-verduras.jpg" }) {
+    file(relativePath: { eq: "frutas-verduras.jpg" }) {
       childImageSharp {
-        fluid{
+        fluid(quality: 100) {
           ...GatsbyImageSharpFluid
         }
       }
     }
   }
-`
-
+`;
 
 export default Home;
