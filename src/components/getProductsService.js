673aae270c574723b30f5f0154415d9c7353c972
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2021-01-14 12:45:21 +0100

    solucionado problema blur de gatsby image

--- src/components/getProductsService.js
+++ src/components/getProductsService.js
@@ -19,6 +19,7 @@ import { useStaticQuery, graphql } from "gatsby"
               desription
               imageName
               altText
+              formato
             }
           }
         }
--- src/pages/productos.js
+++ src/pages/productos.js
@@ -1,22 +1,40 @@
 import React from "react";
 import GetProductsService from "../components/getProductsService";
 import GetImage from "../components/getImage";
-import NavBar from "../components/navBar"
-import  Footer from "../components/footer"
+import NavBar from "../components/navBar";
+import Footer from "../components/footer";
+import { Link } from "gatsby";
+import "../styles/productos.css";
+
 let Productos = (props) => {
   console.log(GetProductsService({ category: "frutas" }));
   return (
     <>
-     
-<NavBar/>
-      
-          <GetImage altText="mierda" imageName="bananas.jpg"/>
-              
-                
-<Footer/>
-           
-     
-    
+      <NavBar />
+      <h1>Productos</h1>
+      <p>sleecciona desde medio kilo o desde una unidad!</p>
+      <hr></hr>
+      <section>
+        <div className="productsContainerItems">
+          {GetProductsService({ category: "frutas" }).map((element) => {
+            return (
+              <div key={element.node.id} className="item">
+              <div>
+                <Link to={element.node.frontmatter.slug}>
+                  <GetImage altText={element.node.frontmatter.altText} imageName={element.node.frontmatter.imageName} />
+                </Link>
+                <Link to={element.node.frontmatter.slug}>
+                    <span>{element.node.frontmatter.name}</span>
+                </Link>
+                {element.node.frontmatter.formato ==="kg" ? <span>{element.node.frontmatter.price}€/Kg</span> : <span>{element.node.frontmatter.price}€/Ud</span>}
+                </div>
+
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <Footer />
     </>
   );
 };
