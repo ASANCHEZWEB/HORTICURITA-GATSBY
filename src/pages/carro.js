8931529cbc92b885652add0a17ec27d3fe3bcce5
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2021-01-29 23:20:09 +0100

    añadido listado de productos  del carro en carro page y animacion y eliminacion de productos desde el carro (muy guay)

--- src/pages/carro.js
+++ src/pages/carro.js
@@ -9,13 +9,14 @@ import { Link } from "gatsby";
 class Carro extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { arrayProducts: [] ,modalHtml:""};
+    this.state = { arrayProducts: [] ,modalInfo:""};
 
     this.getCarritoData = this.getCarritoData.bind(this);
     this.deleteProduct=this.deleteProduct.bind(this);
     this.addProduct=this.addProduct.bind(this);
     this.restProd=this.restProd.bind(this);
     this.deleteModal=this.deleteModal.bind(this)
+    this.cerrarModal=this.cerrarModal.bind(this)
   }
 
   getCarritoData() {
@@ -28,85 +29,41 @@ deleteProduct(productId){
     this.getCarritoData()
 }
 
+deleteModal(product,type){
+let arrayDeleteBox= this.state.arrayProducts.map((element)=>{
+    if(element.node.id===product.node.id){element.node.delete=true}else{element.node.delete=false}
+    return element
+})
+
+this.setState({arrayProducts:arrayDeleteBox,modalInfo:type})
+
+}
+
+
+
 addProduct(product){
     addToLocalStorage(product,true)
-this.getCarritoData()
+    this.getCarritoData()
 
 }
 
-deleteModal(product,state,index){
-    if(state){
-        this.deleteProduct(product.node.id)
-    }else{
+
+cerrarModal(element, deleteElement) {
+    if (deleteElement === false) {
         this.getCarritoData()
     }
-    
-
+    if (deleteElement === true) {
+        this.deleteProduct(element.node.id)
+    }
 }
 
-
-restProd(product,index,type){
+restProd(product){
     if(product.node.frontmatter.agregado!==1){
         restProduct(product,true)
         this.getCarritoData()
     }else{
-if(type==="modal"){
-    let divHtml=<div key={product.node.id} className="modalDeleteCart animate__animated animate__pulse animate__infinite">
-    <Link to={product.node.frontmatter.slug}><GetImage imageName={product.node.frontmatter.imageName[0]} altText={product.node.frontmatter.altText}/></Link>
-     
-<span role="img" aria-labelledby="imagen asombro">¿Cantidad 0?😱</span>
-<span>¿Desea eliminar {product.node.frontmatter.name}?😭</span>
-<div>
-<button onClick={()=>this.deleteModal(product,false,index)} type="button" aria-label="Eliminar">¡No!</button>
-<button onClick={()=>this.deleteModal(product,true,index)} type="button" aria-label="Eliminar">Si</button>
-</div>
-
-
-
-</div>
-let elementDeleteBol=this.state.arrayProducts.filter((element)=>{
-if(element.node.id===product.node.id){
-    element.node.delete=true
-}else{
-    element.node.delete=false
-}
-return element
-})
-
-
-this.setState({modalHtml:divHtml,arrayProducts:elementDeleteBol})
-
-}else{
-    let divHtml=<div key={product.node.id} className="modalDeleteCart animate__animated animate__pulse animate__infinite">
-   <Link to={product.node.frontmatter.slug}><GetImage imageName={product.node.frontmatter.imageName[0]} altText={product.node.frontmatter.altText}/></Link>
-<span role="img" aria-labelledby="imagen asombro">¿Cantidad 0?😱</span>
-<span>¿Desea eliminar {product.node.frontmatter.name}?😭</span>
-<div>
-<button onClick={()=>this.deleteModal(product,false,index)} type="button" aria-label="Eliminar">¡No!</button>
-<button onClick={()=>this.deleteModal(product,true,index)} type="button" aria-label="Eliminar">Si</button>
-</div>
-
-
-
-</div>
-let elementDeleteBol=this.state.arrayProducts.filter((element)=>{
-if(element.node.id===product.node.id){
-    element.node.delete=true
-}else{
-    element.node.delete=false
-}
-return element
-})
-
-
-this.setState({modalHtml:divHtml,arrayProducts:elementDeleteBol})
-
-
-}
-
-
+        this.deleteModal(product,"rest")
     }
-
 }
 
 
@@ -132,29 +89,27 @@ this.setState({modalHtml:divHtml,arrayProducts:elementDeleteBol})
             <div className="displayItemsContainer">{this.state.arrayProducts.map((element,index)=>{
 
 return element.node.delete===false?<div className="itemCart" key={element.node.id}>
-   <div><button onClick={()=>this.restProd(element,index,"delete")} type="button" aria-label="Eliminar"><GetImage imageName="icono-eliminar.png" altText="icono eliminar producto"/></button></div>
+<div><button onClick={()=>this.deleteModal(element,"delete")} type="button" aria-label="Eliminar"><GetImage imageName="icono-eliminar.png" altText="icono eliminar producto"/></button></div>
 <div className="imageItemCart"> <Link to={element.node.frontmatter.slug}><GetImage imageName={element.node.frontmatter.imageName[0]} altText={element.node.frontmatter.altText}/></Link></div>
 <div className="displayItemData"><span>PRODUCTO: </span><Link to={element.node.frontmatter.slug}><span>{element.node.frontmatter.name}</span></Link></div>
 {element.node.frontmatter.formato==="unidades"? <div className="displayItemData"><span>PRECIO/Ud: </span><span>{element.node.frontmatter.price}/Ud</span></div>: <div className="displayItemData"><span>PRECIO/Kg: </span><span>{element.node.frontmatter.price}/Kg</span></div>}
 <div className="displayItemData">
 <span>CANTIDAD: </span>
-
 <div className="containerButtonsCart"><button onClick={()=>this.restProd(element,index,"modal")}>-</button>{element.node.frontmatter.formato==="unidades"? <span>{element.node.frontmatter.agregado}</span>: <span>{element.node.frontmatter.agregado/2}</span>}<button onClick={()=>this.addProduct(element)}>+</button></div>
 </div>
 {element.node.frontmatter.formato==="unidades"? <div className="displayItemData"><strong>SUBTOTAL: </strong><span>{element.node.frontmatter.price*element.node.frontmatter.agregado}€</span></div>: <div className="displayItemData"><strong>SUBTOTAL: </strong><span>{(element.node.frontmatter.price*element.node.frontmatter.agregado/2)}€</span></div>}
-
-  
-
-
-
-</div> :this.state.modalHtml
-
-    
-
-
-
-                     
-                })}</div>
+</div> 
+:
+<div key={element.node.id} className="modalDeleteCart animate__animated animate__pulse animate__infinite">
+   <Link to={element.node.frontmatter.slug}><GetImage imageName={element.node.frontmatter.imageName[0]} altText={element.node.frontmatter.altText}/></Link>
+<strong role="img" aria-labelledby="imagen asombro">{this.state.modalInfo==="delete"?"¿Está seguro?":"¿Cantidad 0?😱"}</strong>
+<span>¿Desea eliminar {element.node.frontmatter.name} del carrito?😢</span>
+<div>
+<button onClick={()=>this.cerrarModal(element,false)} type="button" aria-label="Eliminar">¡No!</button>
+<button onClick={()=>this.cerrarModal(element,true)} type="button" aria-label="Eliminar">¡Sí!</button>
+</div>
+</div>})}
+</div>
                 
             </div>
             <div className="totalCartPay"></div>
