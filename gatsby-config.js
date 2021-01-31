c3f585aeee073150a079e6540b8a73af297adbbb
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2021-01-31 22:06:03 +0100

    añadido env

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-env-variables`,
       options: {
-        allowList: ["SALUDO"]
+        allowList: ["CODIGOSDESCUENTO"]
       },
     }
   ],
